27f57cd40641a09f6078a2a92e131a805ad6d5e4
wcarthur <craig.arthur@ga.gov.au>
2016-01-18 08:02:01 +1100

    Initial commit
    Initial commit of code for extracting gust observations from BoM
    half-hourly station records that correspond to passage of a cyclone within
    a given distance of the station.

---
+++ tc_TC_wnds_stns.r
@@ -0,0 +1,116 @@
+#Title: Program 'tc_TC_wnds_stns.r'
+#Author: Sanabria A., augusto.sanabria@ga.gov.au
+#CreationDate: 2013-10-04
+#Description: Program to extract cyclonic winds from BoM stations
+#located in the cyclonic region of Australia.
+#It reads a dataset of cyclonic tracks affecting a BoM recording station
+#and extracts corresponding values from the station wind speeds.
+#Dataset includes max wind speed of each cyclone coming closer than 600 km to
+#the station.
+#Output is written to external file using station ID as identifier.
+#Keywords: BoM recording stations, BoM database of TC tracks, observed cyclonic winds
+ 
+#Reference:
+#SeeAlso:
+#Version: 1.1
+#Modified by: AS
+#ModifiedDate: 2014-01-10
+#Modification: The cyclonic wind dataset now includes only max wind speeds per cyclone.
+
+#Required:
+#sdir <<- "/home/asanabri/R/my_rsoftw/"
+#source(paste(sdir,"get_TC_gusts.r",sep="")  )
+#source(paste(sdir,"get_TC_gusts_per_cyclone.r",sep="")  )
+source('get_TC_gusts_per_cyclone.r')
+#Returns:
+#void, produces external file.
+#
+t1 <- proc.time()
+#Read database of stations affected by TCs:
+indir <- "/media/nHD2/TC_project_09_13/"
+
+outdir <- "/media/nHD2/TC_project_09_13/new_TC_dist_600k/"
+#N.B. Change non-readable characters [ ] by " " (stn name should be read as a single string)
+
+fname <- paste(indir, "stn_TC_dist.txt", sep="")
+bom_stns_wTC <- read.table(fname, skip=1, head=F)
+
+#Read BoM datasets (half-hour) located in Dir 'datadir'
+datadir <- "/media/nHD2/TC_project_09_13/BoM_datasets/half_hourly_gust/"
+
+setwd(datadir)
+all_bomd <- dir(pattern="HM01X_Data")
+#List stns not in bom data:
+outf001 <- "stns_not_in_bomd.txt"
+write.table("List of stations not in BoM-provided datasets",
+            file=paste(outdir,outf001,sep=""),
+            append=FALSE)
+write.table("stn_ID stn_name stn_ST stn_lat stn_lon",
+            file=paste(outdir,outf001,sep=""),
+            append=TRUE)
+
+#Transform BoM stn_ID in stn name into numeric:
+bom_substr <- c()
+for(i in 1:length(all_bomd) ){
+   bom_substr[i] <- as.numeric(substring(all_bomd[i], 12, 17))
+   }
+#Split the data into stations, extract wind gusts at the time of the TC track approaching the station and
+#create a dataset of TC winds at that station:
+grouped_data <- split(bom_stns_wTC,bom_stns_wTC$V2)
+stn_char <- c()
+cnt_stns <- 0
+all_TC_gusts <- c()
+for(i in 1:length(grouped_data) ){
+#   extract wind gust from BoM dataset
+    stn_id <- grouped_data[[i]][2]
+    stn_ST <- unique(grouped_data[[i]][3])
+    stn_name <- unique(grouped_data[[i]][1])
+    cycl_name <- grouped_data[[i]][6]
+    stn_lat <- grouped_data[[i]][4]
+    stn_lon <- grouped_data[[i]][5]
+    stn_char[i] <- paste(i, "  ", unique(stn_id),
+                         " [",stn_name$V1,"] ", stn_ST$V3,
+                         " (",unique(stn_lat), ",", unique(stn_lon),
+                         ")", sep="")
+#   open corresponding BoM dataset:
+    bdatas <- which(as.numeric(unique(stn_id)) == as.numeric(bom_substr))
+#   Test for errors (n.b. character(0) is numeric!!)
+    ZZ <- try(ifelse(bdatas >= 1, bdatas, NA))
+#   Write to external file list of stations not found in BoM-provided datasets:
+    if(!is.numeric(ZZ)){
+      write.table(stn_char[i],
+                  file=paste(outdir,outf001,sep=""),
+                  append=TRUE, row.names=FALSE, 
+                  col.names=FALSE,quote=FALSE)
+      next
+      }
+#   Write all stns found in BoM-provided datasets to R log file (to plot them in the OZ map):
+    str1 <- paste(unique(stn_id),", ",stn_name$V1,",",unique(stn_lat),",",unique(stn_lon),",above, y",sep="")  
+    print(str1)
+#    TC_gusts <- get_TC_gusts(all_bomd[bdatas],unique(stn_ST),grouped_data[[i]][8],grouped_data[[i]][9])
+#   Split data into cyclones in order to calc. max speed per cyclone:
+    cycl_wsp <- cbind(grouped_data[[i]][8],grouped_data[[i]][9],grouped_data[[i]][6])
+    cycl_mx_wsp <- split(cycl_wsp,cycl_wsp$V6,drop=TRUE)
+#   loop tr' all cyclones:
+    for(j in 1:length(cycl_mx_wsp) ){
+       TC_gusts <- get_TC_gusts_per_cyclone(all_bomd[bdatas],unique(stn_ST),cycl_mx_wsp[[j]][1],cycl_mx_wsp[[j]][2],cycl_mx_wsp[[j]][3] )
+       if(is.na(TC_gusts))next
+#   Keep only max wind speed of this cyclone:
+       mx_gwsp <- which(TC_gusts$V17 == max(TC_gusts$V17,na.rm=TRUE)  )
+#       all_TC_gusts <- try(rbind(all_TC_gusts,TC_gusts[mx_gwsp,]  )  )
+       all_TC_gusts <- try(rbind(all_TC_gusts,TC_gusts )  ) #write all wnd speeds not only max
+    }
+#   Write to external file winds in BoM-provided datasets which match date/time in cyclone track,
+#   one dataset per station ID:
+    cnt_stns <- cnt_stns + 1
+#   write name with 6 digits (as in BoM station name convention):
+    new_str <- formatC(unique(grouped_data[[i]]$V2),width=6,flag="0")
+    out_main <- paste("bom_",new_str,".csv",sep="") 
+    write.csv(all_TC_gusts,file=paste(outdir,out_main,sep="") )
+    all_TC_gusts <- c()
+}
+etime <- proc.time() - t1
+print(paste("Stations in BoM-provided datasets hit by TC = ",cnt_stns,sep="")  )
+print(paste("Proc. time = ",etime,sep=""))
+#
+print("Normal program termination")
---
+++ tc_match_stns.r
@@ -0,0 +1,102 @@
+#Title: Program 'tc_match_stns.r'
+#Author: Sanabria A., augusto.sanabria@ga.gov.au
+#CreationDate: 2013-09-19
+#Description: Program to match BoM stations with TC tracks.
+#The station characteristics are read from BoM station database
+#TC tracks are also provided by BoM.
+#This script compares station location with TC tracks, if the TC track comes
+#closer than 'mx_d' km to the station and the station was in operation at the time,
+#write the station name and location; TC distance to the station, date and TC radius
+#(MN_RADIUS_SF_WND) into an external file ("stn_TC_dist.txt").
+
+#Keywords: BoM recording stations, BoM database of TC tracks
+ 
+#Reference:
+#SeeAlso:
+#Version: 1.0
+#Modified by:
+#ModifiedDate:
+#Modification:
+
+#Required:
+#NIL
+#Returns:
+#void, produces external file.
+#
+#
+#Read TC tracks:
+tc_fname <- "IDCKMSTM0S.csv"
+TC_BoM_datab <- read.csv(tc_fname,skip=3,head=T)
+#Extract fields of interest:
+OZ_TC_datab <- with(TC_BoM_datab,data.frame(name=NAME,UTC_time=TM,lon=LON,lat=LAT,radius=MN_RADIUS_GF_WIND)  )
+
+#Read list of BoM stns and location:
+#stns_list <- "TC_region_stn_list.csv"
+stns_list <- "BoM_stns.txt"
+BoM_stn_list <- read.csv(stns_list,head=T)
+OZ_TCregion_stns <- with(BoM_stn_list,data.frame(stn_ID=Site,stn=Sitename,state=State,lat=Lat,lon=Lon,start=Start,endt=End) )
+#Write all stations which are closer than 'mx_d' km to a TC track and are open at the time of the TC pass
+#Write also stations not included in the two rules listed above with the reason (as a check)
+t1 <- proc.time()
+#mx_d <- 200.0
+#mx_d <- 400.0
+mx_d <- 600.0
+keep_name <- c()
+outf <- "stn_TC_dist.txt"
+outf00 <- "No_TC_hit_stn.txt"
+write.table("Database of BoM stations affected by TC",file=outf,append=FALSE)
+write.table("Database of BoM stations NOT affected by TC",file=outf00,append=FALSE)
+write.table("Stn name BoM_ID State  Stn location TC_name  dist (km)   date passed  TC Radius (km)",file=outf,append=TRUE)
+for(i in 1:length(OZ_TCregion_stns$stn) ){
+  stn_ID <- OZ_TCregion_stns[i,1]
+  stn_name <- OZ_TCregion_stns[i,2]
+  stn_state <- OZ_TCregion_stns[i,3]
+  print(stn_name) 
+  stn_lat <- OZ_TCregion_stns[i,4]*(pi/180.0)
+  stn_lon <- OZ_TCregion_stns[i,5]*(pi/180.0)
+  stn_start <- as.integer(substring(OZ_TCregion_stns[i,6],4,7))
+  if (substring(OZ_TCregion_stns[i,7],4,7) == "    "){
+    stn_close = 5000
+  }else{
+    stn_close = as.integer(substring(OZ_TCregion_stns[i,7],4,7))
+  }
+  #stn_close <- ifelse(as.integer(substring(OZ_TCregion_stns[i,7],3,7)) <= 0,5000,OZ_TCregion_stns[i,7] )
+  for(k in 1:length(OZ_TC_datab$name) ){
+     lat2 <- OZ_TC_datab[k,4]*(pi/180.0)
+     lon2 <- OZ_TC_datab[k,3]*(pi/180.0)
+     if(is.na(lat2) | is.na(lon2) ){
+         print("**Warning: empty line (ignored)")
+         print(paste("TC_name(previous) = ",OZ_TC_datab[k-1,1],sep="")  )
+#         stop (13)
+         next
+     }
+     #print(OZ_TC_datab[k,1])
+     dd <- 2*asin(sqrt((sin((stn_lat-lat2)/2))^2 + 
+                        cos(stn_lat)*cos(lat2)*(sin((stn_lon-lon2)/2))^2)) 
+     dist_2stn <- 1.852*(180*60/pi)*dd
+     #print(paste("dist = ",dist_2stn,sep="") )
+     if(dist_2stn <= mx_d){
+            keep_name <- as.character(OZ_TC_datab[k,1])
+            TC_yy <- as.integer(substring(OZ_TC_datab[k,2],1,4)  )
+            TC_mm <- as.integer(substring(OZ_TC_datab[k,2],6,7)  )
+            if(TC_yy >= stn_start & TC_yy <= stn_close ){
+            #Station is close enough, record station, distance and time of hit:
+                    write.table(paste("[",as.character(stn_name),"] ",stn_ID," ",stn_state," ",signif((180.0/pi)*stn_lat,8)," ",signif((180.0/pi)*stn_lon,8),"  ",keep_name,"  ",round(dist_2stn,1),"  ",
+                    OZ_TC_datab[k,2],"  ",OZ_TC_datab[k,5],sep=""),file=outf,append=TRUE,row.names=FALSE,col.names=FALSE,quote=FALSE)
+            }else{
+            #Station was NOT open at time of TC hit:
+                    write.table(paste(as.character(stn_name)," (",signif((180.0/pi)*stn_lat,8),",",signif((180.0/pi)*stn_lon,8),")  ",keep_name,"  ",round(dist_2stn,1),"  ",OZ_TC_datab[k,2],"  No time match!",sep=""),
+                    file=outf00,append=TRUE,row.names=FALSE,col.names=FALSE,quote=FALSE)
+            }
+     }else{
+            #Station is farther than 'mx_d' km, write it in to 'No_TC_hit' file:
+            write.table(paste(stn_name," (",signif((180.0/pi)*stn_lat,8),",",signif((180.0/pi)*stn_lon,8),")  ",keep_name,"  ",round(dist_2stn,1),"  ",OZ_TC_datab[k,2],"  Too far!",sep=""),
+            file=outf00,append=TRUE,row.names=FALSE,col.names=FALSE)
+
+     }
+  }
+}
+etime <- proc.time() - t1
+print(paste("Proc. time = ",etime,sep="")   )
+print("Normal program termination")
+
