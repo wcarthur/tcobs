5543aee908db3ff5d20bafe810e6c50afd539459
wcarthur <craig.arthur@ga.gov.au>
2016-01-19 14:08:14 +1100

    First changes
    - Only select a single record per TC event (even if there are multiple
    records with the same maximum gust wind speed)
    - Capture any obs that occur within (-60, +90) mins of TC record
    - Includes general clean-up of the code (for readability)

--- get_TC_gusts_per_cyclone.r
+++ get_TC_gusts_per_cyclone.r
@@ -1,14 +1,14 @@
-#function to extract gusts from a BoM dataset at a usr-provided time
-#keeping track of the cyclone which orginated those gust winds.
-#(called from program 'tc_TC_wnds_stns.r')
-get_TC_gusts_per_cyclone = function(bom_dset,ST,date,time,cyclone){
+# function to extract gusts from a BoM dataset at a usr-provided time
+# keeping track of the cyclone which orginated those gust winds.
+# (called from program 'tc_TC_wnds_stns.r')
+get_TC_gusts_per_cyclone = function(bom_dset, ST, date, time, cyclone){
 #bom_dset = name of BoM-provided wind dataset
 #ST = state where dataset is located
 #date = date at which TC hit the station
 #time = corresponding time
 #cyclone = Cyclone name
 #open dataset:
-obs_gusts <- read.csv(bom_dset,skip=1,head=FALSE)
+obs_gusts <- read.csv(bom_dset, skip = 1, header = FALSE)
 #Read Local Standard Time (LST) in BoM dataset:
 ds_yr <- obs_gusts$V8
 ds_m <- obs_gusts$V9
@@ -16,51 +16,38 @@ ds_d <- obs_gusts$V10
 ds_hr <- obs_gusts$V11
 ds_min <- obs_gusts$V12
 #build the LST datetime string:
-ds_dattim1 <- ISOdatetime(ds_yr,ds_m,ds_d,ds_hr,ds_min,sec=0,tz="GMT")
+ds_dattim1 <- ISOdatetime(ds_yr, ds_m, ds_d, ds_hr, ds_min, sec = 0, tz = "GMT")
 #Transform LST to UTC time (to match TC dataset):
-if(ST == "WA"){ds_dattim <- ds_dattim1 - 8.*3600
-}else if(ST == "NT"){ds_dattim <- ds_dattim1 - 9.5*3600
-}else{ds_dattim <- ds_dattim1 - 10*3600
+if (ST == "WA") {
+  ds_dattim <- ds_dattim1 - 8.*3600
+}else if (ST == "NT") {
+  ds_dattim <- ds_dattim1 - 9.5*3600
+}else{
+  ds_dattim <- ds_dattim1 - 10*3600
 }
-#Format it as '1992-10-28':
-ds_dvec <-  format(ds_dattim,"%Y-%m-%d:%H:%M")
-#Format usr_provided time (extracted from database of TC track):
-usr_yr <- substring(date$V8,7,10)
-usr_m <- substring(date$V8,4,5)
-usr_d <- substring(date$V8,1,2)
-usr_xx <- substring(time$V9,1,2)
-usr_hr <- c()
-usr_min <- c()
-for(i in 1:length(usr_xx)){
-   if(substring(usr_xx[i],2,2) == ":"){usr_hr[i] <- paste("0",substring(usr_xx[i],1,1),sep="")
-                                 usr_min[i] <- substring(time$V9[i],3,4)
-   }else{
-       usr_hr[i] <- usr_xx[i]
-       usr_min[i] <- substring(time$V9[i],4,5)
-   }
+
+tcdt = strptime(date$V8, format = "%Y-%m-%d %H:%M:%S", tz = "GMT")
+
+if (max(tcdt) < min(ds_dattim)) {
+  print(paste("No obs available for cyclone: ", as.character(cyclone[1,1]), ": ", max(tcdt)))
+  return(NA)
 }
-#TC track comes in 3-hr intervals. For each track time select also times in BoM datasets +- 30min, +- 60min + 90min
-usr_dattim1 <- ISOdatetime(usr_yr,usr_m,usr_d,usr_hr,usr_min,sec=0,tz="GMT")
-#Append time to final vector:
-usr_dattim <- usr_dattim1
-usr_dattim <- append(usr_dattim,usr_dattim1+30*3600,after=length(usr_dattim)  )
-usr_dattim <- append(usr_dattim,usr_dattim1-30*3600,after=length(usr_dattim)  )
-usr_dattim <- append(usr_dattim,usr_dattim1+60*3600,after=length(usr_dattim)  )
-usr_dattim <- append(usr_dattim,usr_dattim1-60*3600,after=length(usr_dattim)  )
-usr_dattim <- append(usr_dattim,usr_dattim1+90*3600,after=length(usr_dattim)  )
-#
-usr_dvec <-  format(usr_dattim,"%Y-%m-%d:%H:%M")
-##
+obsidx = c()
+for (i in 1:length(tcdt)) {
+  dtm = tcdt[i] - 3600
+  dtp = tcdt[i] + 5400
+  idx = which((ds_dattim >= dtm) & (ds_dattim <= dtp))
+  obsidx = append(obsidx, idx, after = length(obsidx))
+}
+
 #Select only wind records at given times:
-wnd_gusts <- obs_gusts[which(ds_dvec %in% usr_dvec),]
-if(length(wnd_gusts$V1) < 1)return(NA)
+wnd_gusts <- obs_gusts[(obsidx),]
+if (length(wnd_gusts$V1) < 1) return(NA)
 #Build up vector of name to match wnd_gusts:
 NN <- cyclone[1,1]
 cycl_name <- c()
 cycl_name[1:length(wnd_gusts$V1)] <- as.character(NN)
 #Return cyclonic wind speeds (same format as BoM dataset) plus cyclone name in last column:
-cbind(wnd_gusts,cycl_name)
+cbind(wnd_gusts, cycl_name)
 
-
-#
 }
--- tc_match_stns.r
+++ tc_match_stns.r
@@ -10,7 +10,7 @@
 #(MN_RADIUS_SF_WND) into an external file ("stn_TC_dist.txt").
 
 #Keywords: BoM recording stations, BoM database of TC tracks
- 
+
 #Reference:
 #SeeAlso:
 #Version: 1.0
@@ -25,16 +25,22 @@
 #
 #
 #Read TC tracks:
-tc_fname <- "IDCKMSTM0S.csv"
-TC_BoM_datab <- read.csv(tc_fname,skip=3,head=T)
-#Extract fields of interest:
-OZ_TC_datab <- with(TC_BoM_datab,data.frame(name=NAME,UTC_time=TM,lon=LON,lat=LAT,radius=MN_RADIUS_GF_WIND)  )
-
+#tc_fname <- "IDCKMSTM0S.csv"
+#TC_BoM_datab <- read.csv(tc_fname,skip=3,head=T)
+# Extract fields of interest:
+#OZ_TC_datab <- with(TC_BoM_datab,data.frame(name=NAME,UTC_time=TM,lon=LON,lat=LAT,radius=MN_RADIUS_GF_WIND)  )
+tc_fname <- "Allstorms.ibtracs_wmo.v03r06.csv"
+TC_datab <- read.csv(tc_fname, skip = 1, header = T)
+OZ_TC_datab <- with(TC_datab,data.frame(name = Name, UTC_time = ISO_time,
+                                        lon = Longitude, lat = Latitude,
+                                        radius = Pressure))
 #Read list of BoM stns and location:
 #stns_list <- "TC_region_stn_list.csv"
-stns_list <- "BoM_stns.txt"
-BoM_stn_list <- read.csv(stns_list,head=T)
-OZ_TCregion_stns <- with(BoM_stn_list,data.frame(stn_ID=Site,stn=Sitename,state=State,lat=Lat,lon=Lon,start=Start,endt=End) )
+stns_list <- "BoM_stns_short.txt"
+BoM_stn_list <- read.csv(stns_list, header = T)
+OZ_TCregion_stns <- with(BoM_stn_list, data.frame(stn_ID = Site, stn = Sitename,
+                                                  state = State, lat = Lat, lon = Lon,
+                                                  start = Start, endt = End) )
 #Write all stations which are closer than 'mx_d' km to a TC track and are open at the time of the TC pass
 #Write also stations not included in the two rules listed above with the reason (as a check)
 t1 <- proc.time()
@@ -42,61 +48,65 @@ t1 <- proc.time()
 #mx_d <- 400.0
 mx_d <- 600.0
 keep_name <- c()
-outf <- "stn_TC_dist.txt"
-outf00 <- "No_TC_hit_stn.txt"
-write.table("Database of BoM stations affected by TC",file=outf,append=FALSE)
-write.table("Database of BoM stations NOT affected by TC",file=outf00,append=FALSE)
-write.table("Stn name BoM_ID State  Stn location TC_name  dist (km)   date passed  TC Radius (km)",file=outf,append=TRUE)
-for(i in 1:length(OZ_TCregion_stns$stn) ){
-  stn_ID <- OZ_TCregion_stns[i,1]
-  stn_name <- OZ_TCregion_stns[i,2]
-  stn_state <- OZ_TCregion_stns[i,3]
-  print(stn_name) 
-  stn_lat <- OZ_TCregion_stns[i,4]*(pi/180.0)
-  stn_lon <- OZ_TCregion_stns[i,5]*(pi/180.0)
+outf <- "stn_TC_dist.ibtracs.txt"
+#outf00 <- "No_TC_hit_stn.ibtracs.txt"
+write.table("Database of BoM stations affected by TC", file = outf, append = FALSE)
+#write.table("Database of BoM stations NOT affected by TC",file=outf00,append=FALSE)
+write.table("Stn name BoM_ID State  Stn location TC_name  dist (km)   date passed  TC Radius (km)", file = outf, append = TRUE)
+for (i in 1:length(OZ_TCregion_stns$stn)) {
+  stn_ID <- OZ_TCregion_stns[i, 1]
+  stn_name <- OZ_TCregion_stns[i, 2]
+  stn_state <- OZ_TCregion_stns[i, 3]
+  print(stn_name)
+  stn_lat <- OZ_TCregion_stns[i, 4]*(pi/180.0)
+  stn_lon <- OZ_TCregion_stns[i, 5]*(pi/180.0)
   stn_start <- as.integer(substring(OZ_TCregion_stns[i,6],4,7))
-  if (substring(OZ_TCregion_stns[i,7],4,7) == "    "){
+  if ((substring(OZ_TCregion_stns[i,7],4,7) == "    ") | (is.na(OZ_TCregion_stns[i,7]))) {
     stn_close = 5000
   }else{
-    stn_close = as.integer(substring(OZ_TCregion_stns[i,7],4,7))
+    stn_close = as.integer(substring(OZ_TCregion_stns[i, 7], 4, 7))
   }
   #stn_close <- ifelse(as.integer(substring(OZ_TCregion_stns[i,7],3,7)) <= 0,5000,OZ_TCregion_stns[i,7] )
-  for(k in 1:length(OZ_TC_datab$name) ){
-     lat2 <- OZ_TC_datab[k,4]*(pi/180.0)
-     lon2 <- OZ_TC_datab[k,3]*(pi/180.0)
-     if(is.na(lat2) | is.na(lon2) ){
-         print("**Warning: empty line (ignored)")
-         print(paste("TC_name(previous) = ",OZ_TC_datab[k-1,1],sep="")  )
-#         stop (13)
-         next
-     }
-     #print(OZ_TC_datab[k,1])
-     dd <- 2*asin(sqrt((sin((stn_lat-lat2)/2))^2 + 
-                        cos(stn_lat)*cos(lat2)*(sin((stn_lon-lon2)/2))^2)) 
-     dist_2stn <- 1.852*(180*60/pi)*dd
-     #print(paste("dist = ",dist_2stn,sep="") )
-     if(dist_2stn <= mx_d){
-            keep_name <- as.character(OZ_TC_datab[k,1])
-            TC_yy <- as.integer(substring(OZ_TC_datab[k,2],1,4)  )
-            TC_mm <- as.integer(substring(OZ_TC_datab[k,2],6,7)  )
-            if(TC_yy >= stn_start & TC_yy <= stn_close ){
-            #Station is close enough, record station, distance and time of hit:
-                    write.table(paste("[",as.character(stn_name),"] ",stn_ID," ",stn_state," ",signif((180.0/pi)*stn_lat,8)," ",signif((180.0/pi)*stn_lon,8),"  ",keep_name,"  ",round(dist_2stn,1),"  ",
-                    OZ_TC_datab[k,2],"  ",OZ_TC_datab[k,5],sep=""),file=outf,append=TRUE,row.names=FALSE,col.names=FALSE,quote=FALSE)
-            }else{
-            #Station was NOT open at time of TC hit:
-                    write.table(paste(as.character(stn_name)," (",signif((180.0/pi)*stn_lat,8),",",signif((180.0/pi)*stn_lon,8),")  ",keep_name,"  ",round(dist_2stn,1),"  ",OZ_TC_datab[k,2],"  No time match!",sep=""),
-                    file=outf00,append=TRUE,row.names=FALSE,col.names=FALSE,quote=FALSE)
-            }
-     }else{
-            #Station is farther than 'mx_d' km, write it in to 'No_TC_hit' file:
-            write.table(paste(stn_name," (",signif((180.0/pi)*stn_lat,8),",",signif((180.0/pi)*stn_lon,8),")  ",keep_name,"  ",round(dist_2stn,1),"  ",OZ_TC_datab[k,2],"  Too far!",sep=""),
-            file=outf00,append=TRUE,row.names=FALSE,col.names=FALSE)
-
-     }
+  for (k in 1:length(OZ_TC_datab$name)) {
+    lat2 <- OZ_TC_datab[k, 4]*(pi/180.0)
+    lon2 <- OZ_TC_datab[k, 3]*(pi/180.0)
+    if (is.na(lat2) | is.na(lon2) ) {
+      print("**Warning: empty line (ignored)")
+      print(paste("TC_name(previous) = ", OZ_TC_datab[k - 1, 1], sep = ""))
+      next
+    }
+    #print(OZ_TC_datab[k,1])
+    dd <- 2*asin(sqrt((sin((stn_lat - lat2)/2)) ^ 2 +
+                      cos(stn_lat)*cos(lat2)*(sin((stn_lon - lon2)/2)) ^ 2))
+    dist_2stn <- 1.852*(180*60/pi)*dd
+    #print(paste("dist = ",dist_2stn,sep="") )
+    if (dist_2stn <= mx_d) {
+      keep_name <- as.character(OZ_TC_datab[k, 1])
+      TC_yy <- as.integer(substring(OZ_TC_datab[k, 2], 1, 4))
+      TC_mm <- as.integer(substring(OZ_TC_datab[k, 2], 6, 7))
+      if (TC_yy >= stn_start & TC_yy <= stn_close) {
+        #Station is close enough, record station, distance and time of hit:
+        write.table(paste(as.character(stn_name),
+                          stn_ID, stn_state,
+                          signif((180.0/pi)*stn_lat, 8),
+                          signif((180.0/pi)*stn_lon, 8),
+                          keep_name,round(dist_2stn, 1),
+                          OZ_TC_datab[k, 2], OZ_TC_datab[k, 5], sep = ","),
+                    file = outf, append = TRUE, row.names = FALSE,
+                    col.names = FALSE, quote = FALSE)
+      }else{
+       #Station was NOT open at time of TC hit:
+       #write.table(paste(as.character(stn_name)," (",signif((180.0/pi)*stn_lat,8),",",signif((180.0/pi)*stn_lon,8),")  ",keep_name,"  ",round(dist_2stn,1),"  ",OZ_TC_datab[k,2],"  No time match!",sep=""),
+       #file=outf00,append=TRUE,row.names=FALSE,col.names=FALSE,quote=FALSE)
+      }
+    }else{
+      #Station is farther than 'mx_d' km, write it in to 'No_TC_hit' file:
+      #write.table(paste(stn_name," (",signif((180.0/pi)*stn_lat,8),",",signif((180.0/pi)*stn_lon,8),")  ",keep_name,"  ",round(dist_2stn,1),"  ",OZ_TC_datab[k,2],"  Too far!",sep=""),
+      #file=outf00,append=TRUE,row.names=FALSE,col.names=FALSE)
+    }
   }
 }
 etime <- proc.time() - t1
-print(paste("Proc. time = ",etime,sep="")   )
+print(paste("Proc. time = ", etime, sep = ""))
 print("Normal program termination")
 
